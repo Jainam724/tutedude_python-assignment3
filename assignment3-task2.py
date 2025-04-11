aaece016a00b9cb13d9ed2e6e7d1b4b412418cfd
Jainam Gandhi <103090341+Jainam724@users.noreply.github.com>
2025-04-11 19:36:48 +0530

    Add files via upload

---
+++ assignment3-task2.py
@@ -0,0 +1,10 @@
+import math
+
+num = int(input("Enter a number: "))
+sqrt = math.sqrt(num)
+log = math.log(num)
+sine = math.sin(num)
+
+print(f"Square root of {num} is {sqrt}")
+print(f"Logarithm of {num} is {log}")
+print(f"Sine of {num} is {sine}")
